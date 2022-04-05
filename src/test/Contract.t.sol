1087b44f6a722797791fc6e3626cd1c990e6b2b9
Oliver Chalk <oliverchalk245@gmail.com>
2022-04-05 22:27:55 +0800

    feat: add Uniswap/v2-core contracts un-modified

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.10;
+pragma solidity =0.5.16;
 
 import "ds-test/test.sol";
+
+import { UniswapV2Factory } from "src/UniswapV2Factory.sol";
 
 contract ContractTest is DSTest {
     function setUp() public {}
 
     function testExample() public {
+        UniswapV2Factory lFactory = new UniswapV2Factory();
         assertTrue(true);
     }
 }
