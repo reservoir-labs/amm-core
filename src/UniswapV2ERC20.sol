d65d337d45e7fbdcb1216c3168ef0ae011e9d0d1
A.L <xenide@users.noreply.github.com>
2022-04-07 12:40:24 +0800

    fix: replaced SafeMath using builtin operators

--- src/UniswapV2ERC20.sol
+++ src/UniswapV2ERC20.sol
@@ -1,10 +1,8 @@
 pragma solidity =0.8.13;
 
 import './interfaces/IUniswapV2ERC20.sol';
-import './libraries/SafeMath.sol';
 
 contract UniswapV2ERC20 is IUniswapV2ERC20 {
-    using SafeMath for uint;
 
     string public constant name = 'Uniswap V2';
     string public constant symbol = 'UNI-V2';
@@ -35,14 +33,14 @@ contract UniswapV2ERC20 is IUniswapV2ERC20 {
     }
 
     function _mint(address to, uint value) internal {
-        totalSupply = totalSupply.add(value);
-        balanceOf[to] = balanceOf[to].add(value);
+        totalSupply = totalSupply + value;
+        balanceOf[to] = balanceOf[to] + value;
         emit Transfer(address(0), to, value);
     }
 
     function _burn(address from, uint value) internal {
-        balanceOf[from] = balanceOf[from].sub(value);
-        totalSupply = totalSupply.sub(value);
+        balanceOf[from] = balanceOf[from] - value;
+        totalSupply = totalSupply - value;
         emit Transfer(from, address(0), value);
     }
 
@@ -52,8 +50,8 @@ contract UniswapV2ERC20 is IUniswapV2ERC20 {
     }
 
     function _transfer(address from, address to, uint value) private {
-        balanceOf[from] = balanceOf[from].sub(value);
-        balanceOf[to] = balanceOf[to].add(value);
+        balanceOf[from] = balanceOf[from] - value;
+        balanceOf[to] = balanceOf[to] + value;
         emit Transfer(from, to, value);
     }
 
@@ -69,7 +67,7 @@ contract UniswapV2ERC20 is IUniswapV2ERC20 {
 
     function transferFrom(address from, address to, uint value) external returns (bool) {
         if (allowance[from][msg.sender] != uint(2**256-1)) {
-            allowance[from][msg.sender] = allowance[from][msg.sender].sub(value);
+            allowance[from][msg.sender] = allowance[from][msg.sender] - value;
         }
         _transfer(from, to, value);
         return true;
