e4e79fb40878d5ebf110178b8b4068ae16691c38
A.L <xenide@users.noreply.github.com>
2022-05-04 21:38:30 +0800

    feat: import (minimum) dependencies from saddle

---
+++ saddle-tests/hardhat.config.ts
@@ -0,0 +1,296 @@
+import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-web3"
+import "@nomiclabs/hardhat-etherscan"
+import "@typechain/hardhat"
+import "hardhat-gas-reporter"
+import "solidity-coverage"
+import "hardhat-deploy"
+import "hardhat-spdx-license-identifier"
+
+import { HardhatUserConfig, task } from "hardhat/config"
+import dotenv from "dotenv"
+import { ethers } from "ethers"
+import { ALCHEMY_BASE_URL, CHAIN_ID } from "./utils/network"
+import { PROD_DEPLOYER_ADDRESS } from "./utils/accounts"
+import { Deployment } from "hardhat-deploy/dist/types"
+
+dotenv.config()
+
+if (process.env.HARDHAT_FORK) {
+  process.env["HARDHAT_DEPLOY_FORK"] = process.env.HARDHAT_FORK
+}
+
+let config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      deploy: ["./deploy/mainnet/"],
+      autoImpersonate: true,
+    },
+    mainnet: {
+      url: ALCHEMY_BASE_URL[CHAIN_ID.MAINNET] + process.env.ALCHEMY_API_KEY,
+      deploy: ["./deploy/mainnet/"],
+    },
+    ropsten: {
+      url: ALCHEMY_BASE_URL[CHAIN_ID.ROPSTEN] + process.env.ALCHEMY_API_KEY,
+      accounts: {
+        mnemonic: process.env.MNEMONIC_TEST_ACCOUNT,
+      },
+      deploy: ["./deploy/ropsten/"],
+    },
+    arbitrum_testnet: {
+      url:
+          ALCHEMY_BASE_URL[CHAIN_ID.ARBITRUM_TESTNET] +
+          process.env.ALCHEMY_API_KEY,
+      chainId: 421611,
+      accounts: {
+        mnemonic: process.env.MNEMONIC_TEST_ACCOUNT,
+      },
+      deploy: ["./deploy/arbitrum/"],
+    },
+    arbitrum_mainnet: {
+      url:
+          ALCHEMY_BASE_URL[CHAIN_ID.ARBITRUM_MAINNET] +
+          process.env.ALCHEMY_API_KEY,
+      chainId: 42161,
+      gasPrice: ethers.utils.parseUnits("2", "gwei").toNumber(),
+      deploy: ["./deploy/arbitrum/"],
+    },
+    optimism_testnet: {
+      url: "https://kovan.optimism.io",
+      chainId: 69,
+      accounts: {
+        mnemonic: process.env.MNEMONIC_TEST_ACCOUNT,
+      },
+      deploy: ["./deploy/optimism/"],
+    },
+    optimism_mainnet: {
+      url: "https://mainnet.optimism.io",
+      chainId: 10,
+      deploy: ["./deploy/optimism/"],
+    },
+    fantom_testnet: {
+      url: "https://rpc.testnet.fantom.network/",
+      chainId: 4002,
+      accounts: {
+        mnemonic: process.env.MNEMONIC_TEST_ACCOUNT,
+      },
+      deploy: ["./deploy/fantom/"],
+    },
+    fantom_mainnet: {
+      url: "https://rpc.ftm.tools/",
+      chainId: 250,
+      deploy: ["./deploy/fantom/"],
+    },
+    evmos_testnet: {
+      url: "https://eth.bd.evmos.dev:8545",
+      chainId: 9000,
+      deploy: ["./deploy/evmos_testnet/"],
+      accounts: {
+        mnemonic: process.env.MNEMONIC_TEST_ACCOUNT,
+      },
+    },
+    evmos_mainnet: {
+      url: "https://eth.bd.evmos.org:8545",
+      chainId: 9001,
+      deploy: ["./deploy/evmos/"],
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    artifacts: "./build/artifacts",
+    cache: "./build/cache",
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 10000,
+          },
+        },
+      },
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 10000,
+          },
+        },
+      },
+      {
+        version: "0.5.16",
+      },
+    ],
+    overrides: {
+      "contracts/helper/Multicall3.sol": {
+        version: "0.8.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 10000000,
+          },
+        },
+      },
+    },
+  },
+  typechain: {
+    outDir: "./build/typechain/",
+    target: "ethers-v5",
+  },
+  gasReporter: {
+    currency: "USD",
+    gasPrice: 21,
+  },
+  mocha: {
+    timeout: 200000,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+      42161: 0, // use the same address on arbitrum mainnet
+      10: 0, // use the same address on optimism mainnet
+      250: 0, // use the same address on fantom mainnet
+      9000: 0, // use the same address on evmos testnet
+      9001: 0, // use the same address on evmos mainnnet
+      3: 0, // use the same address on ropsten
+    },
+    libraryDeployer: {
+      default: 1, // use a different account for deploying libraries on the hardhat network
+      1: 0, // use the same address as the main deployer on mainnet
+      42161: 0, // use the same address on arbitrum mainnet
+      10: 0, // use the same address on optimism mainnet
+      250: 0, // use the same address on fantom mainnet
+      9000: 0, // use the same address on evmos testnet
+      9001: 0, // use the same address on evmos mainnnet
+      3: 0, // use the same address on ropsten
+    },
+  },
+  spdxLicenseIdentifier: {
+    overwrite: false,
+    runOnCompile: true,
+  },
+}
+
+if (process.env.ETHERSCAN_API) {
+  config = { ...config, etherscan: { apiKey: process.env.ETHERSCAN_API } }
+}
+
+if (process.env.ACCOUNT_PRIVATE_KEYS) {
+  config.networks = {
+    ...config.networks,
+    mainnet: {
+      ...config.networks?.mainnet,
+      accounts: JSON.parse(process.env.ACCOUNT_PRIVATE_KEYS),
+    },
+    arbitrum_mainnet: {
+      ...config.networks?.arbitrum_mainnet,
+      accounts: JSON.parse(process.env.ACCOUNT_PRIVATE_KEYS),
+    },
+    optimism_mainnet: {
+      ...config.networks?.optimism_mainnet,
+      accounts: JSON.parse(process.env.ACCOUNT_PRIVATE_KEYS),
+    },
+    fantom_mainnet: {
+      ...config.networks?.fantom_mainnet,
+      accounts: JSON.parse(process.env.ACCOUNT_PRIVATE_KEYS),
+    },
+    evmos_mainnet: {
+      ...config.networks?.evmos_mainnet,
+      accounts: JSON.parse(process.env.ACCOUNT_PRIVATE_KEYS),
+    },
+  }
+}
+
+if (process.env.FORK_MAINNET === "true" && config.networks) {
+  console.log("FORK_MAINNET is set to true")
+  config = {
+    ...config,
+    networks: {
+      ...config.networks,
+      hardhat: {
+        ...config.networks.hardhat,
+        forking: {
+          url: process.env.ALCHEMY_API_KEY
+              ? ALCHEMY_BASE_URL[CHAIN_ID.MAINNET] + process.env.ALCHEMY_API_KEY
+              : throwAPIKeyNotFoundError(),
+        },
+        chainId: 1,
+      },
+    },
+    namedAccounts: {
+      ...config.namedAccounts,
+      deployer: {
+        1: PROD_DEPLOYER_ADDRESS,
+      },
+    },
+    external: {
+      deployments: {
+        localhost: ["deployments/mainnet"],
+      },
+    },
+  }
+}
+
+function throwAPIKeyNotFoundError(): string {
+  throw Error("ALCHEMY_API_KEY environment variable is not set")
+  return ""
+}
+
+// Override the default deploy task
+task("deploy", async (taskArgs, hre, runSuper) => {
+  const { all } = hre.deployments
+  /*
+   * Pre-deployment actions
+   */
+
+  // Load exiting deployments
+  const existingDeployments: { [p: string]: Deployment } = await all()
+  // Create hard copy of existing deployment name to address mapping
+  const existingDeploymentToAddressMap: { [p: string]: string } = Object.keys(
+      existingDeployments,
+  ).reduce((acc: { [p: string]: string }, key) => {
+    acc[key] = existingDeployments[key].address
+    return acc
+  }, {})
+
+  /*
+   * Run super task
+   */
+  await runSuper(taskArgs)
+
+  /*
+   * Post-deployment actions
+   */
+  const updatedDeployments: { [p: string]: Deployment } = await all()
+
+  // Filter out any existing deployments that have not changed
+  const newDeployments: { [p: string]: Deployment } = Object.keys(
+      updatedDeployments,
+  ).reduce((acc: { [p: string]: Deployment }, key) => {
+    if (
+        !existingDeploymentToAddressMap.hasOwnProperty(key) ||
+        existingDeploymentToAddressMap[key] !== updatedDeployments[key].address
+    ) {
+      acc[key] = updatedDeployments[key]
+    }
+    return acc
+  }, {})
+
+  // Print the new deployments to the console
+  if (Object.keys(newDeployments).length > 0) {
+    console.log("\nNew deployments:")
+    console.table(
+        Object.keys(newDeployments).map((k) => [k, newDeployments[k].address]),
+    )
+  } else {
+    console.warn("\nNo new deployments found")
+  }
+})
+
+export default config
