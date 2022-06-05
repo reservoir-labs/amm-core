65aa91b2b487c17e1591fb076b06db80488fefca
A.L <xenide@users.noreply.github.com>
2022-06-05 12:37:22 +0100

    feat: getting sushi legacy tests to work

---
+++ src/mocks/ERC20Mock.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >= 0.8.0;
+
+import "@openzeppelin/token/ERC20/ERC20.sol";
+import "@openzeppelin/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+contract ERC20Mock is ERC20Permit {
+    constructor(string memory name, string memory symbol, uint256 supply) ERC20(name, symbol) ERC20Permit(name) {
+        _mint(msg.sender, supply);
+    }
+}
