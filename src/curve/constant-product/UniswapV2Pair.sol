f7b060c36a31306aa0ab43936d51a0af490c4051
A.L <xenide@users.noreply.github.com>
2022-06-16 23:40:40 +0100

    fix: keccak simplification

--- src/curve/constant-product/UniswapV2Pair.sol
+++ src/curve/constant-product/UniswapV2Pair.sol
@@ -13,7 +13,7 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
     using UQ112x112 for uint224;
 
     uint public constant MINIMUM_LIQUIDITY = 10**3;
-    bytes4 private constant SELECTOR = bytes4(keccak256(bytes("transfer(address,uint256)")));
+    bytes4 private constant SELECTOR = bytes4(keccak256("transfer(address,uint256)"));
     // Accuracy^2: 10_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000
     uint256 public constant SQUARED_ACCURACY = 1e76;
     // Accuracy: 100_000_000_000_000_000_000_000_000_000_000_000_000
