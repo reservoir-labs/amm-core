05c376ae7bbf514202c5060365525cb33ac7e867
A.L <xenide@users.noreply.github.com>
2022-06-16 19:54:58 +0100

    test: typing and style changes

--- test/HybridPool.t.sol
+++ test/HybridPool.t.sol
@@ -16,7 +16,7 @@ contract HybridPoolTest is Test
     HybridPoolFactory private mPoolFactory = new HybridPoolFactory(address(mMasterDeployer));
     MintableERC20 private mTokenA = new MintableERC20("TokenA", "TA");
     MintableERC20 private mTokenB = new MintableERC20("TokenB", "TB");
-    address private mPool;
+    HybridPool private mPool;
 
     function setUp() public
     {
@@ -26,73 +26,73 @@ contract HybridPoolTest is Test
     function _createPair() private
     {
         mMasterDeployer.addToWhitelist(address(mPoolFactory));
-        bytes memory deployData = abi.encode(address(mTokenA), address(mTokenB), 25, 1000);
-        mPool = mMasterDeployer.deployPool(address(mPoolFactory), deployData);
+        bytes memory lDeployData = abi.encode(address(mTokenA), address(mTokenB), 25, 1000);
+        mPool = HybridPool(mMasterDeployer.deployPool(address(mPoolFactory), lDeployData));
     }
 
     function testMint() public
     {
         // arrange
-        mTokenA.mint(mPool, 100e18);
-        mTokenB.mint(mPool, 100e18);
+        mTokenA.mint(address(mPool), 100e18);
+        mTokenB.mint(address(mPool), 100e18);
 
         // act
-        uint256 liquidity = HybridPool(mPool).mint(abi.encode(address(this)));
+        uint256 lLiquidity = mPool.mint(abi.encode(address(this)));
 
         // assert
-        assertEq(HybridPool(mPool).balanceOf(address(this)), liquidity);
+        assertEq(mPool.balanceOf(address(this)), lLiquidity);
     }
 
     function testMint_OnlyTransferOneToken() public
     {
         // arrange
-        mTokenA.mint(mPool, 100e18);
+        mTokenA.mint(address(mPool), 100e18);
 
         // act & assert
         vm.expectRevert(stdError.divisionError);
-        HybridPool(mPool).mint(abi.encode(address(this)));
+        mPool.mint(abi.encode(address(this)));
     }
 
     function testSwap() public
     {
         // arrange
-        mTokenA.mint(mPool, 100e18);
-        mTokenB.mint(mPool, 100e18);
-        HybridPool(mPool).mint(abi.encode(address(this)));
+        mTokenA.mint(address(mPool), 100e18);
+        mTokenB.mint(address(mPool), 100e18);
+        mPool.mint(abi.encode(address(this)));
 
         // act
-        mTokenA.mint(address(mPool), 5e18);
-        uint256 amountOut = HybridPool(mPool).swap(abi.encode(address(mTokenA), address(this)));
+        mTokenA.mint(address(address(mPool)), 5e18);
+        uint256 lAmountOut = mPool.swap(abi.encode(address(mTokenA), address(this)));
 
         // assert
-        assertEq(amountOut, mTokenB.balanceOf(address(this)));
+        assertEq(lAmountOut, mTokenB.balanceOf(address(this)));
     }
 
     function testSwap_NoTransferTokens() public
     {
         // arrange
-        mTokenA.mint(mPool, 100e18);
-        mTokenB.mint(mPool, 100e18);
-        HybridPool(mPool).mint(abi.encode(address(this)));
+        mTokenA.mint(address(mPool), 100e18);
+        mTokenB.mint(address(mPool), 100e18);
+        mPool.mint(abi.encode(address(this)));
 
         // act & assert
         vm.expectRevert("UniswapV2: TRANSFER_FAILED");
-        HybridPool(mPool).swap(abi.encode(address(mTokenA), address(this)));
+        mPool.swap(abi.encode(address(mTokenA), address(this)));
     }
 
     function testBurn() public
     {
         // arrange
-        mTokenA.mint(mPool, 100e18);
-        mTokenB.mint(mPool, 100e18);
-        HybridPool(mPool).mint(abi.encode(address(this)));
+        mTokenA.mint(address(mPool), 100e18);
+        mTokenB.mint(address(mPool), 100e18);
+        mPool.mint(abi.encode(address(this)));
 
         // act
-        HybridPool(mPool).transfer(mPool, HybridPool(mPool).balanceOf(address(this)));
-        HybridPool(mPool).burn(abi.encode(address(this)));
+        mPool.transfer(address(mPool), mPool.balanceOf(address(this)));
+        mPool.burn(abi.encode(address(this)));
 
         // assert
-        assertEq(HybridPool(mPool).balanceOf(address(this)), 0);
+        assertEq(mPool.balanceOf(address(this)), 0);
         assertEq(mTokenA.balanceOf(address(this)), 99999999999999999500);
         assertEq(mTokenB.balanceOf(address(this)), 99999999999999999500);
     }
