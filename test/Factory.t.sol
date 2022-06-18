8be42e23b6a3b591034e8723ba80f54fcf3f76dd
A.L <xenide@users.noreply.github.com>
2022-06-18 11:50:03 +0100

    test: refactored to move test cases to more appropriate files

--- test/Factory.t.sol
+++ test/Factory.t.sol
@@ -26,38 +26,9 @@ contract FactoryTest is Test
         mPair = createPair(mTokenA, mTokenB);
     }
 
-    function calculateOutput(
-        uint256 aReserveIn,
-        uint256 aReserveOut,
-        uint256 aTokenIn,
-        uint256 aFee
-    ) private pure returns (uint256 rExpectedOut)
-    {
-        uint256 lAmountInWithFee = aTokenIn * (10_000 - aFee);
-        uint256 lNumerator = lAmountInWithFee * aReserveOut;
-        uint256 lDenominator = aReserveIn * 10_000 + lAmountInWithFee;
-
-        rExpectedOut = lNumerator / lDenominator;
-    }
-
-    function getToken0Token1(address aTokenA, address aTokenB) private pure returns (address rToken0, address rToken1)
-    {
-        (rToken0, rToken1) = aTokenA < aTokenB ? (aTokenA, aTokenB) : (aTokenB, aTokenA);
-    }
-
     function createPair(MintableERC20 aTokenA, MintableERC20 aTokenB) private returns (UniswapV2Pair rPair)
     {
         rPair = UniswapV2Pair(mFactory.createPair(address(aTokenA), address(aTokenB)));
-    }
-
-    function provideLiquidity(address aPair) private
-    {
-        mTokenA.mint(address(this), 100e18);
-        mTokenB.mint(address(this), 100e18);
-
-        mTokenA.transfer(aPair, 100e18);
-        mTokenB.transfer(aPair, 100e18);
-        UniswapV2Pair(aPair).mint(address(this));
     }
 
     function testCreatePair() public
@@ -67,40 +38,5 @@ contract FactoryTest is Test
 
         // assert
         assertEq(mFactory.allPairs(1), address(pair));
-    }
-
-    function testLiquidityProvision() public
-    {
-        // act
-        provideLiquidity(address(mPair));
-
-        // assert
-        uint256 lpTokenBalance = mPair.balanceOf(address(this));
-        assertEq(lpTokenBalance, 99999999999999999000);
-        assertEq(mTokenA.balanceOf(address(this)), 0);
-        assertEq(mTokenB.balanceOf(address(this)), 0);
-    }
-
-    function testSwap() public
-    {
-        // arrange
-        provideLiquidity(address(mPair));
-
-        uint256 reserve0;
-        uint256 reserve1;
-        (reserve0, reserve1, ) = mPair.getReserves();
-        uint256 expectedOutput = calculateOutput(reserve0, reserve1, 1e18, 30);
-
-        // act
-        address token0;
-        address token1;
-        (token0, token1) = getToken0Token1(address(mTokenA), address(mTokenB));
-
-        MintableERC20(token0).mint(address(mPair), 1e18);
-        mPair.swap(0, expectedOutput, address(this), "");
-
-        // assert
-        assertEq(MintableERC20(token1).balanceOf(address(this)), expectedOutput);
-        assertEq(MintableERC20(token0).balanceOf(address(this)), 0);
     }
 }
--- test/Pair.t.sol
+++ test/Pair.t.sol
@@ -21,12 +21,41 @@ contract PairTest is Test
     function setUp() public
     {
         mFactory = new UniswapV2Factory(30, 2500, mOwner, mRecoverer);
-        createPair();
+        mPair = _createPair(mTokenA, mTokenB);
     }
 
-    function createPair() private
+    function _createPair(MintableERC20 aTokenA, MintableERC20 aTokenB) private returns (UniswapV2Pair rPair)
     {
-        mPair = UniswapV2Pair(mFactory.createPair(address(mTokenA), address(mTokenB)));
+        rPair = UniswapV2Pair(mFactory.createPair(address(aTokenA), address(aTokenB)));
+    }
+
+    function _provideLiquidity(address aPair) private
+    {
+        mTokenA.mint(address(this), 100e18);
+        mTokenB.mint(address(this), 100e18);
+
+        mTokenA.transfer(aPair, 100e18);
+        mTokenB.transfer(aPair, 100e18);
+        UniswapV2Pair(aPair).mint(address(this));
+    }
+
+    function _calculateOutput(
+        uint256 aReserveIn,
+        uint256 aReserveOut,
+        uint256 aTokenIn,
+        uint256 aFee
+    ) private pure returns (uint256 rExpectedOut)
+    {
+        uint256 lAmountInWithFee = aTokenIn * (10_000 - aFee);
+        uint256 lNumerator = lAmountInWithFee * aReserveOut;
+        uint256 lDenominator = aReserveIn * 10_000 + lAmountInWithFee;
+
+        rExpectedOut = lNumerator / lDenominator;
+    }
+
+    function _getToken0Token1(address aTokenA, address aTokenB) private pure returns (address rToken0, address rToken1)
+    {
+        (rToken0, rToken1) = aTokenA < aTokenB ? (aTokenA, aTokenB) : (aTokenB, aTokenA);
     }
 
     function testCustomSwapFeeOffByDefault() public
@@ -115,5 +144,45 @@ contract PairTest is Test
         // assert
         assertEq(mPair.swapFee(), 200);
         assertEq(mPair.platformFee(), 5000);
+    }
+
+    function testMint() public
+    {
+        // act
+        _provideLiquidity(address(mPair));
+
+        // assert
+        uint256 lpTokenBalance = mPair.balanceOf(address(this));
+        assertEq(lpTokenBalance, 99999999999999999000);
+        assertEq(mTokenA.balanceOf(address(this)), 0);
+        assertEq(mTokenB.balanceOf(address(this)), 0);
+    }
+
+    function testSwap() public
+    {
+        // arrange
+        _provideLiquidity(address(mPair));
+
+        uint256 reserve0;
+        uint256 reserve1;
+        (reserve0, reserve1, ) = mPair.getReserves();
+        uint256 expectedOutput = _calculateOutput(reserve0, reserve1, 1e18, 30);
+
+        // act
+        address token0;
+        address token1;
+        (token0, token1) = _getToken0Token1(address(mTokenA), address(mTokenB));
+
+        MintableERC20(token0).mint(address(mPair), 1e18);
+        mPair.swap(0, expectedOutput, address(this), "");
+
+        // assert
+        assertEq(MintableERC20(token1).balanceOf(address(this)), expectedOutput);
+        assertEq(MintableERC20(token0).balanceOf(address(this)), 0);
+    }
+
+    function testBurn() public
+    {
+
     }
 }
