9e0a550d2bb0cc6b4870c5d9f61134c9489011fe
A.L <xenide@users.noreply.github.com>
2022-07-08 01:04:48 +0200

    fix: alignment for require statement

--- src/curve/stable/HybridPool.sol
+++ src/curve/stable/HybridPool.sol
@@ -103,8 +103,10 @@ contract HybridPool is UniswapV2ERC20, ReentrancyGuard {
         require(token0 != address(0), "ZERO_ADDRESS");
         require(token0 != token1, "IDENTICAL_ADDRESSES");
         require(swapFee >= MIN_SWAP_FEE && swapFee <= MAX_SWAP_FEE, "INVALID_SWAP_FEE");
-        require(ampData.initialA >= StableMath.MIN_A * uint64(StableMath.A_PRECISION)
-             && ampData.initialA <= StableMath.MAX_A * uint64(StableMath.A_PRECISION), "INVALID_A");
+        require(
+            ampData.initialA >= StableMath.MIN_A * uint64(StableMath.A_PRECISION)
+            && ampData.initialA <= StableMath.MAX_A * uint64(StableMath.A_PRECISION), "INVALID_A"
+        );
     }
 
     function setCustomSwapFee(uint _customSwapFee) external onlyFactory {
