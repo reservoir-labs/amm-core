896ceb726bad87c2e58cd564ffbd6d8b5e5f18b0
A.L <xenide@users.noreply.github.com>
2022-07-21 23:43:25 +0200

    test: separate unit and integration tests

--- test/AssetManager.t.sol
+++ test/AssetManager.t.sol
@@ -7,7 +7,7 @@ import { IERC20 } from "@openzeppelin/interfaces/IERC20.sol";
 import { CTokenInterface } from "src/interfaces/CErc20Interface.sol";
 import { MathUtils } from "src/libraries/MathUtils.sol";
 
-contract AssetManagerTest is BaseTest
+contract AssetManagerIntegration is BaseTest
 {
 
     address public constant ETH_MAINNET_CUSDC = address(0x39AA39c021dfbaE8faC545936693aC917d5E7563);
