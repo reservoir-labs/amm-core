29efa1268b2f8e761a7fb5d1e3fd872a7eeb938c
andersonaguiar <revsoft10@gmail.com>
2022-07-25 13:49:04 +1000

    fix #41

--- src/curve/constant-product/UniswapV2Pair.sol
+++ src/curve/constant-product/UniswapV2Pair.sol
@@ -1,4 +1,4 @@
-pragma solidity =0.8.13;
+pragma solidity 0.8.13;
 
 import "@openzeppelin/token/ERC20/IERC20.sol";
 import "@openzeppelin/utils/math/SafeCast.sol";
--- src/interfaces/IAssetManager.sol
+++ src/interfaces/IAssetManager.sol
@@ -1,5 +1,5 @@
 /* solhint-disable reason-string */
-pragma solidity =0.8.13;
+pragma solidity 0.8.13;
 
 interface IAssetManager {
     function getBalance(address owner, address token) external returns (uint112 tokenBalance);
