e34bf6c2a3c97b45378e9a15d29adbf3ace99bf9
A.L <xenide@users.noreply.github.com>
2022-08-30 20:34:09 +0200

    fix: use assert instead of require for OracleMath

--- src/libraries/OracleMath.sol
+++ src/libraries/OracleMath.sol
@@ -49,7 +49,7 @@ library OracleMath {
         }
 
         int256 rawResult = LogCompression.toLowResLog(spotPrice);
-        require(rawResult >= type(int112).min && rawResult <= type(int112).max, "OM: EXPONENT_GREATER_THAN_INT112");
+        assert(rawResult >= type(int112).min && rawResult <= type(int112).max);
         logSpotPrice = int112(rawResult);
     }
 
