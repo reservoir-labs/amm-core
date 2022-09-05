511d2e1f9b1a3e63c6a640599b9635a914a4448d
A.L <xenide@users.noreply.github.com>
2022-09-05 13:50:05 +0200

    test: add tests for asset manager integration for StablePair

--- test/integration/Aave.t.sol
+++ test/integration/Aave.t.sol
@@ -7,6 +7,7 @@ import { IERC20 } from "@openzeppelin/interfaces/IERC20.sol";
 import { IPoolAddressesProvider } from "src/interfaces/aave/IPoolAddressesProvider.sol";
 import { IPool } from "src/interfaces/aave/IPool.sol";
 import { IAaveProtocolDataProvider } from "src/interfaces/aave/IAaveProtocolDataProvider.sol";
+import { IAssetsManagedPair } from "src/interfaces/IAssetsManagedPair.sol";
 
 import { MathUtils } from "src/libraries/MathUtils.sol";
 import { AaveManager } from "src/asset-manager/AaveManager.sol";
@@ -24,16 +25,21 @@ contract AaveIntegrationTest is BaseTest
 
     function setUp() public
     {
-        // mint some USDC to this address
         deal(FTM_USDC, address(this), MINT_AMOUNT, true);
-
         _constantProductPair = ConstantProductPair(_createPair(address(_tokenA), FTM_USDC, 0));
         IERC20(FTM_USDC).transfer(address(_constantProductPair), MINT_AMOUNT);
         _tokenA.mint((address(_constantProductPair)), MINT_AMOUNT);
         _constantProductPair.mint(_alice);
-
         vm.prank(address(_factory));
         _constantProductPair.setManager(_manager);
+
+        deal(FTM_USDC, address(this), MINT_AMOUNT, true);
+        _stablePair = StablePair(_createPair(address(_tokenA), FTM_USDC, 1));
+        IERC20(FTM_USDC).transfer(address(_stablePair), MINT_AMOUNT);
+        _tokenA.mint((address(_stablePair)), 1_000_000e18);
+        _stablePair.mint(_alice);
+        vm.prank(address(_factory));
+        _stablePair.setManager(_manager);
     }
 
     function _createOtherPair() private returns (ConstantProductPair rOtherPair)
@@ -267,5 +273,193 @@ contract AaveIntegrationTest is BaseTest
         // act & assert
         vm.expectRevert("AM: INVALID_THRESHOLD");
         _manager.setLowerThreshold(lThreshold);
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                  WITH STABLEPAIR
+    //////////////////////////////////////////////////////////////////////////*/
+
+    function testAdjustManagement_IncreaseManagementOneToken_Stable() public
+    {
+        // arrange
+        int256 lAmountToManage = 500e6;
+
+        // act
+        _manager.adjustManagement(address(_stablePair), lAmountToManage, 0);
+
+        // assert
+        IAaveProtocolDataProvider lDataProvider = _manager.dataProvider();
+        (address lAaveToken, , ) = lDataProvider.getReserveTokensAddresses(_stablePair.token0());
+
+        assertEq(_stablePair.token0Managed(), uint256(lAmountToManage));
+        assertEq(IERC20(FTM_USDC).balanceOf(address(_stablePair)), MINT_AMOUNT - uint256(lAmountToManage));
+        assertEq(IERC20(lAaveToken).balanceOf(address(_manager)), uint256(lAmountToManage));
+        assertEq(_manager.shares(address(_stablePair), _stablePair.token0()), uint256(lAmountToManage));
+        assertEq(_manager.totalShares(lAaveToken), uint256(lAmountToManage));
+    }
+
+    function testAdjustManagement_DecreaseManagementOneToken_Stable() public
+    {
+        // arrange
+        int256 lAmountToManage = 500e6;
+        testAdjustManagement_IncreaseManagementOneToken_Stable();
+
+        // act
+        _manager.adjustManagement(address(_stablePair), -lAmountToManage, 0);
+
+        // assert
+        IAaveProtocolDataProvider lDataProvider = _manager.dataProvider();
+        (address lAaveToken, , ) = lDataProvider.getReserveTokensAddresses(_stablePair.token0());
+
+        assertEq(_stablePair.token0Managed(), 0);
+        assertEq(IERC20(FTM_USDC).balanceOf(address(_stablePair)), MINT_AMOUNT);
+        assertEq(IERC20(lAaveToken).balanceOf(address(this)), 0);
+        assertEq(_manager.shares(address(_stablePair), address(FTM_USDC)), 0);
+        assertEq(_manager.totalShares(lAaveToken), 0);
+    }
+
+    function testAdjustManagement_DecreaseManagementBeyondShare_Stable() public
+    {
+        // arrange
+        ConstantProductPair lOtherPair = _createOtherPair();
+        int256 lAmountToManage1 = 500e6;
+        int256 lAmountToManage2 = 500e6;
+
+        _manager.adjustManagement(address(_stablePair), lAmountToManage1, 0);
+        _manager.adjustManagement(address(lOtherPair), lAmountToManage2, 0);
+
+        // act & assert
+        vm.expectRevert(stdError.arithmeticError);
+        _manager.adjustManagement(address(lOtherPair), -lAmountToManage2-1, 0);
+    }
+
+    function testGetBalance_Stable(uint256 aAmountToManage) public
+    {
+        // arrange
+        (uint256 lReserve0, , ) = _stablePair.getReserves();
+        int256 lAmountToManage = int256(bound(aAmountToManage, 0, lReserve0));
+        _manager.adjustManagement(address(_stablePair), lAmountToManage, 0);
+
+        // act
+        uint112 lBalance = _manager.getBalance(address(_stablePair), FTM_USDC);
+
+        // assert
+        assertTrue(MathUtils.within1(lBalance, uint256(lAmountToManage)));
+    }
+
+    function testGetBalance_NoShares_Stable(address aToken) public
+    {
+        // arrange
+        vm.assume(aToken != FTM_USDC);
+        int256 lAmountToManage = 500e6;
+        _manager.adjustManagement(address(_stablePair), lAmountToManage, 0);
+
+        // act
+        uint256 lRes = _manager.getBalance(address(_stablePair), aToken);
+
+        // assert
+        assertEq(lRes, 0);
+    }
+
+    function testGetBalance_TwoPairsInSameMarket_Stable(uint256 aAmountToManage1, uint256 aAmountToManage2) public
+    {
+        // arrange
+        ConstantProductPair lOtherPair = _createOtherPair();
+        (uint256 lReserve0, , ) = _stablePair.getReserves();
+        int256 lAmountToManage1 = int256(bound(aAmountToManage1, 1, lReserve0));
+        int256 lAmountToManage2 = int256(bound(aAmountToManage2, 1, lReserve0));
+
+        // act
+        _manager.adjustManagement(address(_stablePair), lAmountToManage1, 0);
+        _manager.adjustManagement(address(lOtherPair), lAmountToManage2, 0);
+
+        // assert
+        assertTrue(MathUtils.within1(_manager.getBalance(address(_stablePair), FTM_USDC), uint256(lAmountToManage1)));
+        assertTrue(MathUtils.within1(_manager.getBalance(address(lOtherPair), FTM_USDC), uint256(lAmountToManage2)));
+    }
+
+    function testGetBalance_AddingAfterExchangeRateChange_Stable(
+        uint256 aAmountToManage1,
+        uint256 aAmountToManage2,
+        uint256 aTime
+    ) public
+    {
+        // arrange
+        ConstantProductPair lOtherPair = _createOtherPair();
+        (address lAaveToken, , ) = _manager.dataProvider().getReserveTokensAddresses(_stablePair.token0());
+        (uint256 lReserve0, , ) = _stablePair.getReserves();
+        int256 lAmountToManage1 = int256(bound(aAmountToManage1, 1, lReserve0));
+        _manager.adjustManagement(address(_stablePair), lAmountToManage1, 0);
+
+        // act
+        skip(bound(aTime, 1, 52 weeks));
+        uint256 lAaveTokenAmt2 = IERC20(lAaveToken).balanceOf(address(_manager));
+        int256 lAmountToManage2 = int256(bound(aAmountToManage2, 1, lReserve0));
+        _manager.adjustManagement(address(lOtherPair), lAmountToManage2, 0);
+
+        // assert
+        assertEq(_manager.shares(address(_stablePair), FTM_USDC), uint256(lAmountToManage1));
+        assertTrue(MathUtils.within1(_manager.getBalance(address(_stablePair), FTM_USDC), lAaveTokenAmt2));
+
+        uint256 lExpectedShares
+        = uint256(lAmountToManage2) * 1e18
+        / (lAaveTokenAmt2 * 1e18 / uint256(lAmountToManage1));
+        assertEq(_manager.shares(address(lOtherPair), FTM_USDC), lExpectedShares);
+        assertTrue(MathUtils.within1(_manager.getBalance(address(lOtherPair), FTM_USDC), uint256(lAmountToManage2)));
+    }
+
+    function testShares_Stable(uint256 aAmountToManage) public
+    {
+        // arrange
+        IAaveProtocolDataProvider lDataProvider = _manager.dataProvider();
+        (address lAaveToken, , ) = lDataProvider.getReserveTokensAddresses(_stablePair.token0());
+        (uint256 lReserve0, , ) = _stablePair.getReserves();
+        int256 lAmountToManage = int256(bound(aAmountToManage, 0, lReserve0));
+        _manager.adjustManagement(address(_stablePair), lAmountToManage, 0);
+
+        // act
+        uint256 lShares = _manager.shares(address(_stablePair), FTM_USDC);
+        uint256 lTotalShares = _manager.totalShares(lAaveToken);
+
+        // assert
+        assertEq(lShares, lTotalShares);
+        assertEq(lShares, uint256(lAmountToManage));
+        assertEq(lTotalShares, uint256(lAmountToManage));
+    }
+
+    function testCallback_IncreaseInvestmentAfterMint_Stable() public
+    {
+        // sanity
+        uint256 lAmountManaged = _manager.getBalance(address(_stablePair), FTM_USDC);
+        assertEq(lAmountManaged, 0);
+
+        // act
+        _tokenA.mint(address(_stablePair), 500e6);
+        deal(FTM_USDC, address(this), 500e6, true);
+        IERC20(FTM_USDC).transfer(address(_stablePair), 500e6);
+        _stablePair.mint(address(this));
+
+        // assert
+        uint256 lNewAmount = _manager.getBalance(address(_stablePair), FTM_USDC);
+        (uint256 lReserve0, , ) = _stablePair.getReserves();
+        assertEq(lNewAmount, lReserve0 * (_manager.lowerThreshold() + _manager.upperThreshold()) / 2 / 100);
+    }
+
+    function testCallback_DecreaseInvestmentAfterBurn_Stable(uint256 aInitialAmount) public
+    {
+        // arrange
+        (uint256 lReserve0, , ) = _stablePair.getReserves();
+        uint256 lInitialAmount = bound(aInitialAmount, lReserve0 * (_manager.upperThreshold() + 2) / 100, lReserve0);
+        _manager.adjustManagement(address(_stablePair), int256(lInitialAmount), 0);
+
+        // act
+        vm.prank(_alice);
+        IERC20(address(_stablePair)).transfer(address(_stablePair), 100e6);
+        _stablePair.burn(address(this));
+
+        // assert
+        uint256 lNewAmount = _manager.getBalance(address(_stablePair), FTM_USDC);
+        (uint256 lReserve0After, , ) = _stablePair.getReserves();
+        assertTrue(MathUtils.within1(lNewAmount, lReserve0After * (_manager.lowerThreshold() + _manager.upperThreshold()) / 2 / 100));
     }
 }
