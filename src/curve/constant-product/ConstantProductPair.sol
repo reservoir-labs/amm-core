1807a99f96e8b424805814f2b8065b2826a8d94f
A.L <xenide@users.noreply.github.com>
2022-09-18 17:04:20 -0500

    fix: removed unused code

--- src/curve/constant-product/ConstantProductPair.sol
+++ src/curve/constant-product/ConstantProductPair.sol
@@ -172,37 +172,6 @@ contract ConstantProductPair is ReservoirPair {
         _managerCallback();
     }
 
-    // this low-level function should be called from a contract which performs important safety checks
-//    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external nonReentrant {
-//        require(amount0Out > 0 || amount1Out > 0, "CP: INSUFFICIENT_OUTPUT_AMOUNT");
-//        (uint112 _reserve0, uint112 _reserve1,) = getReserves(); // gas savings
-//        require(amount0Out < _reserve0 && amount1Out < _reserve1, "CP: INSUFFICIENT_LIQ_SWAP");
-//
-//        uint balance0;
-//        uint balance1;
-//        { // scope for _token{0,1}, avoids stack too deep errors
-//            address _token0 = token0;
-//            address _token1 = token1;
-//            require(to != _token0 && to != _token1, "CP: INVALID_TO");
-//            if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
-//            if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
-//            if (data.length > 0) IUniswapV2Callee(to).uniswapV2Call(msg.sender, amount0Out, amount1Out, data);
-//            balance0 = _totalToken0();
-//            balance1 = _totalToken1();
-//        }
-//        uint amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
-//        uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
-//        require(amount0In > 0 || amount1In > 0, "CP: INSUFFICIENT_INPUT_AMOUNT");
-//        { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
-//            uint balance0Adjusted = (balance0 * 10000) - (amount0In * swapFee);
-//            uint balance1Adjusted = (balance1 * 10000) - (amount1In * swapFee);
-//            require(balance0Adjusted * balance1Adjusted >= uint(_reserve0) * _reserve1 * (10000**2), "CP: K");
-//        }
-//
-//        _update(balance0, balance1, _reserve0, _reserve1);
-//        emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
-//    }
-
     /// @inheritdoc IPair
     function swap(int256 amount, bool inOrOut, address to, bytes calldata data) external nonReentrant returns (uint256 amountOut) {
         require(amount != 0, "CP: AMOUNT_ZERO");
