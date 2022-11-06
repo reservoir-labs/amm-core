e5ed5ad83c191192dba4c316acb0778ed030499d
A.L <xenide@users.noreply.github.com>
2022-11-06 01:20:41 -0500

    docs: enhance docs with limits

--- src/interfaces/IOracleWriter.sol
+++ src/interfaces/IOracleWriter.sol
@@ -5,10 +5,14 @@ interface IOracleWriter {
         // natural log (ln) of the raw accumulated price (token1/token0)
         int112 logAccRawPrice;
         // natural log (ln) of the clamped accumulated price (token1/token0)
-        // even in the case of extreme prices (3e75), will overflow once every 878 years
+        // in the case of maximum price supported by the oracle (~2.87e56 == e ** 130.0000)
+        // (1300000) 21 bits multiplied by 32 bits of the timestamp gives 53 bits
+        // which fits into int56
         int56 logAccClampedPrice;
-        // natural log (ln) of the accumulated liquidity (sqrt(k))
-        // even in the case of extreme liquidity (3e75), will overflow once every 878 years
+        // natural log (ln) of the accumulated liquidity (sqrt(x * y))
+        // in the case of maximum liq (sqrt(uint112 * uint112) == 5.192e33 == e ** 77.5325)
+        // (775325) 20 bits multiplied by 32 bits of the timestamp gives 52 bits
+        // which fits into int56
         int56 logAccLiquidity;
         // overflows every 136 years, in the year 2106
         uint32 timestamp;
