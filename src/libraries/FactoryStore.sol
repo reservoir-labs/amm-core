b69b271f9c308e72e936c7486e5f3d4800b7e84a
A.L <xenide@users.noreply.github.com>
2022-11-05 22:16:45 -0500

    feat: library functions for `GenericFactory::set`

--- src/libraries/FactoryStore.sol
+++ src/libraries/FactoryStore.sol
@@ -2,11 +2,43 @@
 pragma solidity 0.8.13;
 
 import { IGenericFactory } from "src/interfaces/IGenericFactory.sol";
+import { Bytes32Lib } from "src/libraries/Bytes32.sol";
 
 library FactoryStoreLib
 {
+    using Bytes32Lib for bytes4;
+    using Bytes32Lib for bool;
+    using Bytes32Lib for uint256;
+    using Bytes32Lib for int256;
+    using Bytes32Lib for address;
+
     function read(IGenericFactory aFactory, string memory aKey) internal view returns (bytes32)
     {
         return aFactory.get(keccak256(abi.encodePacked(aKey)));
+    }
+
+    function write(IGenericFactory aFactory, string memory aKey, bytes4 aValue) internal
+    {
+        aFactory.set(keccak256(abi.encodePacked(aKey)), aValue.toBytes32());
+    }
+
+    function write(IGenericFactory aFactory, string memory aKey, bool aValue) internal
+    {
+        aFactory.set(keccak256(abi.encodePacked(aKey)), aValue.toBytes32());
+    }
+
+    function write(IGenericFactory aFactory, string memory aKey, uint256 aValue) internal
+    {
+        aFactory.set(keccak256(abi.encodePacked(aKey)), aValue.toBytes32());
+    }
+
+    function write(IGenericFactory aFactory, string memory aKey, int256 aValue) internal
+    {
+        aFactory.set(keccak256(abi.encodePacked(aKey)), aValue.toBytes32());
+    }
+
+    function write(IGenericFactory aFactory, string memory aKey, address aValue) internal
+    {
+        aFactory.set(keccak256(abi.encodePacked(aKey)), aValue.toBytes32());
     }
 }
