1d33fad9668fcbbe0b7b1e5c640875e8dc98be58
A.L <xenide@users.noreply.github.com>
2022-11-05 22:25:47 -0500

    fix: delete bytes4 conversion

--- src/libraries/FactoryStore.sol
+++ src/libraries/FactoryStore.sol
@@ -6,7 +6,6 @@ import { Bytes32Lib } from "src/libraries/Bytes32.sol";
 
 library FactoryStoreLib
 {
-    using Bytes32Lib for bytes4;
     using Bytes32Lib for bool;
     using Bytes32Lib for uint256;
     using Bytes32Lib for int256;
@@ -15,11 +14,6 @@ library FactoryStoreLib
     function read(IGenericFactory aFactory, string memory aKey) internal view returns (bytes32)
     {
         return aFactory.get(keccak256(abi.encodePacked(aKey)));
-    }
-
-    function write(IGenericFactory aFactory, string memory aKey, bytes4 aValue) internal
-    {
-        aFactory.set(keccak256(abi.encodePacked(aKey)), aValue.toBytes32());
     }
 
     function write(IGenericFactory aFactory, string memory aKey, bool aValue) internal
