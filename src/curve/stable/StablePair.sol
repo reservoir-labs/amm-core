beb769758a8e4320c09d703bf0a243e3e926d115
A.L <xenide@users.noreply.github.com>
2022-11-05 23:51:47 -0500

    fix: removed console import

--- src/curve/stable/StablePair.sol
+++ src/curve/stable/StablePair.sol
@@ -13,7 +13,7 @@ import { StableMath } from "src/libraries/StableMath.sol";
 import { StableOracleMath } from "src/libraries/StableOracleMath.sol";
 import { ReservoirPair } from "src/ReservoirPair.sol";
 import { IPair, Pair } from "src/Pair.sol";
-import "forge-std/console.sol";
+
 struct AmplificationData {
     /// @dev initialA is stored with A_PRECISION (i.e. multiplied by 100)
     uint64 initialA;
