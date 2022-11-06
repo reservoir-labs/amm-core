cefd2caf636c62a95bdbe5da54430168c8843c7e
A.L <xenide@users.noreply.github.com>
2022-11-06 17:09:39 -0500

    test: percentDelta tests

---
+++ test/unit/libraries/StdMath.t.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+
+contract StdMathTest is Test
+{
+    function testPercentDelta() external
+    {
+        // arrange
+        uint256 lA = 1e18;
+        uint256 lB = 0.5e18;
+
+        // act
+        uint256 lDelta = stdMath.percentDelta(lA, lB);
+
+        // assert
+        assertEq(lDelta, 1e18);
+    }
+
+    function testPercentDelta_PlusOne() external
+    {
+        // arrange
+        uint256 lA = 1e18 + 1;
+        uint256 lB = 0.5e18;
+
+        // act
+        uint256 lDelta = stdMath.percentDelta(lA, lB);
+
+        // assert
+        assertEq(lDelta, 1000000000000000002);
+    }
+
+    function testPercentDelta_MinusOne() external
+    {
+        // arrange
+        uint256 lA = 1e18 - 1;
+        uint256 lB = 0.5e18;
+
+        // act
+        uint256 lDelta = stdMath.percentDelta(lA, lB);
+
+        // assert
+        assertEq(lDelta, 999999999999999998);
+    }
+}
