d4c81a2dc3126755d64e199fc59aaaabae6a539d
A.L <xenide@users.noreply.github.com>
2022-11-29 21:06:26 +0800

    test: got the first differential fuzz working

--- test/differential/LogExpMath.t.sol
+++ test/differential/LogExpMath.t.sol
@@ -11,17 +11,29 @@ contract LogExpMathTest is BaseTest
     function setUp() public
     {
         // we use getDeployedCode since the library contract is stateless
-        bytes memory lBytecode = vm.getDeployedCode("./reference/balancer-v2-monorepo/pkg/solidity-utils/artifacts/contracts/math/LogExpMath.sol/LogExpMath.json");
+        bytes memory lBytecode = vm.getDeployedCode("./reference/balancer-v2-monorepo/pkg/solidity-utils/artifacts/contracts/math/LogExpMathWrapper.sol/LogExpMathWrapper.json");
         vm.etch(_balancerLogExpMath, lBytecode);
     }
 
-    function testPow() external
+    function testPow(uint256 aX, uint256 xY) external
     {
-        uint256 lX = 5;
-        uint256 lY = 6;
+        // assume
+        uint256 lX = bound(aX, 0, type(uint256).max);
+        uint256 lY = bound(xY, 0, type(uint256).max);
 
-        (bool lBool, bytes memory xx) = _balancerLogExpMath.staticcall(abi.encodeWithSignature("pow(uint256,uint256)", lX, lY));
-        uint256 lLocalRes = LogExpMath.pow(lX, lY);
+        // act
+        (bool lBool, bytes memory lRes) = _balancerLogExpMath.staticcall(abi.encodeWithSignature("pow(uint256,uint256)", lX, lY));
+
+        // assert
+        if (lBool) {
+            uint256 lLocalRes = LogExpMath.pow(lX, lY);
+            uint256 lDecoded = abi.decode(lRes, (uint256));
+            assertEq(lLocalRes, lDecoded);
+        }
+        else {
+            vm.expectRevert();
+            LogExpMath.pow(lX, lY);
+        }
     }
 
     function testExp() external
