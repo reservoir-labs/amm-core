b8ea239660293bf609d30ddeb4428277c4ab05d7
A.L <xenide@users.noreply.github.com>
2022-12-27 10:58:28 +0800

    test: prove that test fails for wrong impl

--- src/libraries/Constants.sol
+++ src/libraries/Constants.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.0;
 import { StableMintBurn } from "src/curve/stable/StableMintBurn.sol";
 
 library ConstantsLib {
-    bytes32 public constant MINT_BURN_KEY = bytes32(0x0277d6f3b256c4660b4129f02a2c399534d939de3d721df4efbbe45b1ad6078f);
+    bytes32 public constant MINT_BURN_KEY = bytes32(0x6d51aa069d2ce11858fdce645192f3c54436d537d68e78e0dbb299408fb3d7d4);
 }
