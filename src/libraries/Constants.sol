69f1e6cc7024e2a653184e270a45682eda98adea
A.L <xenide@users.noreply.github.com>
2023-01-04 14:47:48 +0800

    fix: update unoptimized key

--- src/libraries/Constants.sol
+++ src/libraries/Constants.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.0;
 import { StableMintBurn } from "src/curve/stable/StableMintBurn.sol";
 
 library ConstantsLib {
-    bytes32 public constant MINT_BURN_KEY = bytes32(0xa1f3e220b7e4a24ae1ea398c9ce7b421525c3cb9256bbc6e3a8146013a78c695);
+    bytes32 public constant MINT_BURN_KEY = bytes32(0x9f2e3182f7489364c1136baab64ea81449d88833d8f2546be403dc78d2fce652);
 }
