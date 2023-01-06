931af4ad95f83504a0f2dc18cd6a16b81dc70752
A.L <xenide@users.noreply.github.com>
2023-01-07 01:14:42 +0800

    fix: set StableMintBurn key

--- src/libraries/Constants.sol
+++ src/libraries/Constants.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.0;
 import { StableMintBurn } from "src/curve/stable/StableMintBurn.sol";
 
 library ConstantsLib {
-    bytes32 public constant MINT_BURN_KEY = bytes32(0xa1f3e220b7e4a24ae1ea398c9ce7b421525c3cb9256bbc6e3a8146013a78c695);
+    bytes32 public constant MINT_BURN_KEY = bytes32(0x33ea21d206bb39263b8823fb3419790df1f7bd673a7b03f807a2694583dcc9e8);
 }
