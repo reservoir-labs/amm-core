09e9c3342cd8d74cc5000841686712f0e459c4d5
A.L <xenide@users.noreply.github.com>
2023-01-08 23:58:44 +0800

    lint: solhint disable

--- test/__fixtures/MintableERC20.sol
+++ test/__fixtures/MintableERC20.sol
@@ -6,6 +6,7 @@ import { ERC20 } from "solmate/tokens/ERC20.sol";
 contract MintableERC20 is ERC20 {
     uint8 private _decimals;
 
+    // solhint-disable-next-line no-empty-blocks
     constructor(string memory aName, string memory aSymbol, uint8 aDecimals) ERC20(aName, aSymbol, aDecimals) { }
 
     function mint(address aReceiver, uint256 aAmount) external {
