78089734c5f16f1de57e1be2bc8ad802431e9f64
A.L <xenide@users.noreply.github.com>
2023-01-10 18:10:38 +0800

    fix: import path typo

--- src/GenericFactory.sol
+++ src/GenericFactory.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import { Math } from "@openzeppelin/utils/Math/Math.sol";
-
+import { Math } from "@openzeppelin/utils/math/Math.sol";
+import { Address } from "@openzeppelin/utils/Address.sol";
 import { SSTORE2 } from "solmate/utils/SSTORE2.sol";
 import { Owned } from "solmate/auth/Owned.sol";
-import { Address } from "@openzeppelin/utils/Address.sol";
 
 import { IGenericFactory } from "src/interfaces/IGenericFactory.sol";
 
