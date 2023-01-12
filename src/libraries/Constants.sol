7c97aa27c996e881c903d614c58609eedd1707b7
A.L <xenide@users.noreply.github.com>
2023-01-12 20:19:16 +0800

    test: demonstrate problem of managed amount greater than uint104

--- src/libraries/Constants.sol
+++ src/libraries/Constants.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.0;
 import { StableMintBurn } from "src/curve/stable/StableMintBurn.sol";
 
 library ConstantsLib {
-    bytes32 public constant MINT_BURN_KEY = bytes32(0x867764c7a0b7f0d6f72cccaaaf6fdffc416d28081e6dbb8e88436e70294e77bf);
+    bytes32 public constant MINT_BURN_KEY = bytes32(0x11aac052c514692aa6f0d7ea8c2207c8932d4b9198a5e9ced3f14fda3f72b8ce);
 }
