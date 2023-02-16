0cd9015fff61fcdcf7c7ad96d19af867a066784a
A.L <xenide@users.noreply.github.com>
2023-02-16 18:18:16 +0800

    test: add import

--- test/unit/gas/GenericFactory.gas.sol
+++ test/unit/gas/GenericFactory.gas.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+import "test/__fixtures/BaseTest.sol";
+
 contract GenericFactoryGasTest is BaseTest {
     function testCreateStablePair() external {
         _factory.createPair(address(_tokenC), address(_tokenD), 1);
