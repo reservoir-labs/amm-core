b899003e8f3ce1ba0f592df4bfa9a9890441419f
A.L <xenide@users.noreply.github.com>
2023-02-18 14:23:47 +0800

    test: add gas test for creating constant product pair

--- test/unit/gas/GenericFactory.gas.sol
+++ test/unit/gas/GenericFactory.gas.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.0;
 import "test/__fixtures/BaseTest.sol";
 
 contract GenericFactoryGasTest is BaseTest {
+    function testCreateConstantProductPair() external {
+        _factory.createPair(address(_tokenC), address(_tokenD), 0);
+    }
+
     function testCreateStablePair() external {
         _factory.createPair(address(_tokenC), address(_tokenD), 1);
     }
