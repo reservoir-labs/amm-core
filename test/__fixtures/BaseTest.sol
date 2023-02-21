43e91adfb615265caf897352fac1ce7caeb4b3a6
A.L <xenide@users.noreply.github.com>
2023-02-21 19:31:07 +0800

    test: remove unused internal function

--- test/__fixtures/BaseTest.sol
+++ test/__fixtures/BaseTest.sol
@@ -100,21 +100,6 @@ abstract contract BaseTest is Test {
         }
     }
 
-    // From: https://ethereum.stackexchange.com/questions/126899/convert-bytes-to-hexadecimal-string-in-solidity
-    function _bytesToHex(bytes memory buffer) public pure returns (string memory) {
-        // Fixed buffer size for hexadecimal convertion
-        bytes memory converted = new bytes(buffer.length * 2);
-
-        bytes memory _base = "0123456789abcdef";
-
-        for (uint256 i = 0; i < buffer.length; i++) {
-            converted[i * 2] = _base[uint8(buffer[i]) / _base.length];
-            converted[i * 2 + 1] = _base[uint8(buffer[i]) % _base.length];
-        }
-
-        return string(abi.encodePacked("0x", converted));
-    }
-
     function _makeAddress(string memory aName) internal returns (address) {
         address lAddress = address(uint160(uint256(keccak256(abi.encodePacked(aName)))));
         vm.label(lAddress, aName);
