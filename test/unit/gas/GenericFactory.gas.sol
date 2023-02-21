c7fc241d080cd7d4ab35eed1293387120c27c7d3
A.L <xenide@users.noreply.github.com>
2023-02-21 16:31:07 +0800

    test: add gas test for creating factory

--- test/unit/gas/GenericFactory.gas.sol
+++ test/unit/gas/GenericFactory.gas.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.0;
 import "test/__fixtures/BaseTest.sol";
 
 contract GenericFactoryGasTest is BaseTest {
+    function testCreateFactory() external {
+        new GenericFactory(address(this));
+    }
+
     function testCreateConstantProductPair() external {
         _factory.createPair(address(_tokenC), address(_tokenD), 0);
     }
