ac81c800da898dfcffac2df2ffc81fe6f7f34c73
A.L <xenide@users.noreply.github.com>
2023-02-28 00:19:45 +0800

    fix: make all variables private unless necessary

--- src/libraries/Constants.sol
+++ src/libraries/Constants.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.8.0;
 
 library ConstantsLib {
     // TODO: to replace this with the actual production address once the deployer address / key has been decided
-    address public constant MINT_BURN_ADDRESS = 0x0e3B95e1836dd25E4D553Ffb68E7dDd1809eA43A;
+    address public constant MINT_BURN_ADDRESS = 0x17C08f643Af18d35f0B478F8AA998DC65D39873E;
 }
