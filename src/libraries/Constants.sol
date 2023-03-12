e698a4d9e920613ac59e8a16e619217b10df0b15
A.L <xenide@users.noreply.github.com>
2023-03-12 23:04:10 +0800

    fix: update mint burn address

--- src/libraries/Constants.sol
+++ src/libraries/Constants.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 library ConstantsLib {
     // TODO: to replace this with the actual production address once the deployer address / key has been decided
-    address public constant MINT_BURN_ADDRESS = 0xa69E8DF7232756e49EB9Fa8e56c2441154dc0Ff6;
+    address public constant MINT_BURN_ADDRESS = 0x197121B1A2fd8eF22C24ca74F136B5fF3475aE52;
 
     uint256 public constant INITIAL_MINT_AMOUNT = 100e18;
     uint256 public constant DEFAULT_SWAP_FEE_CP = 3000; // 0.3%
