1b945923e5cd16f2af57a1b3993a4afb804e882d
A.L <xenide@users.noreply.github.com>
2023-03-16 20:09:04 +0800

    fix: mint burn key

--- src/libraries/Constants.sol
+++ src/libraries/Constants.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.8.0;
 
 library ConstantsLib {
     // TODO: to replace this with the actual production address once the deployer address / key has been decided
-    address public constant MINT_BURN_ADDRESS = 0xAb5a4e61FdB0eEf6EEF1343FDfbb8f49Bd78cd70;
+    address public constant MINT_BURN_ADDRESS = 0x024d2569c6527B75c5aE105049C59f3816a70D98;
 }
