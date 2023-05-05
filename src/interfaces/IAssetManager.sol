d56b5798022caa7ced938f6ec8d63bcbb47858a5
A.L <xenide@users.noreply.github.com>
2023-05-05 19:24:03 +0100

    docs: for `afterLiquidityEvent` and `returnAsset`

--- src/interfaces/IAssetManager.sol
+++ src/interfaces/IAssetManager.sol
@@ -7,6 +7,12 @@ import { IAssetManagedPair } from "src/interfaces/IAssetManagedPair.sol";
 
 interface IAssetManager {
     function getBalance(IAssetManagedPair owner, ERC20 token) external returns (uint256 tokenBalance);
+
+    /// @notice called by the pair after mint/burn events to automatically re-balance the amount managed
+    /// according to the lower and upper thresholds
     function afterLiquidityEvent() external;
+
+    /// @notice called by the pair when it requires assets managed by the manager to be returned to the pair
+    /// in order to fulfill swap requests or burn requests
     function returnAsset(bool aToken0, uint256 aAmount) external;
 }
