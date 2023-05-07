6b97cc846bc65ad8c19c034d33320faf3d8aa8f3
A.L <xenide@users.noreply.github.com>
2023-05-07 18:27:26 +0100

    docs: change comment into natspec

--- src/GenericFactory.sol
+++ src/GenericFactory.sol
@@ -130,7 +130,7 @@ contract GenericFactory is IGenericFactory, Owned {
 
     event Pair(address indexed token0, address indexed token1, uint256 curveId, address pair);
 
-    // maps tokenA, tokenB addresses to pair address, where the order of tokenA and tokenB does not matter
+    /// @notice maps tokenA, tokenB addresses to pair address, where the order of tokenA and tokenB does not matter
     mapping(address => mapping(address => mapping(uint256 => address))) public getPair;
     address[] private _allPairs;
 
