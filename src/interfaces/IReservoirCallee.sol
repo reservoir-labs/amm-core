58261cba9296d04065c58a43d6d2abc320fc0d39
A.L <xenide@users.noreply.github.com>
2023-05-07 17:42:06 +0100

    docs: improve docs for IReservoirCallee

--- src/interfaces/IReservoirCallee.sol
+++ src/interfaces/IReservoirCallee.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.0;
 
 interface IReservoirCallee {
-    /// @param sender the target address for the arbitrary data call in the case of a flash swap
+    /// @param sender user that initiated the swap and is triggering the callback
     /// @param amount0 positive indicates the amount out (received by callee), negative indicates the amount in (owed by callee)
     /// @param amount1 positive indicates the amount out (received by callee), negative indicates the amount in (owed by callee)
-    /// @param data the bytes that are provided in the case of a flash swap
+    /// @param data provided by the user is returned as part of the callback
     function reservoirCall(address sender, int256 amount0, int256 amount1, bytes calldata data) external;
 }
