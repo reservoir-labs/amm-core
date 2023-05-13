cd406a617f3709ca5fdbd81833d8f7ec656b5734
A.L <xenide@users.noreply.github.com>
2023-05-13 12:38:50 +0100

    feat: mulDiv for getAmountOut

--- src/libraries/ConstantProductMath.sol
+++ src/libraries/ConstantProductMath.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
+import { Math } from "@openzeppelin/utils/math/Math.sol";
+
 library ConstantProductMath {
+    using Math for uint256;
+
     uint256 public constant FEE_ACCURACY = 1_000_000; // 100%
 
     function getAmountOut(uint256 aAmountIn, uint256 aReserveIn, uint256 aReserveOut, uint256 aSwapFee)
@@ -12,9 +16,9 @@ library ConstantProductMath {
         require(aAmountIn > 0, "CP: INSUFFICIENT_INPUT_AMOUNT");
         require(aReserveIn > 0 && aReserveOut > 0, "CP: INSUFFICIENT_LIQUIDITY");
 
-        uint256 lAmountInWithFee = aAmountIn * (FEE_ACCURACY - aSwapFee);
-        uint256 lNumerator = lAmountInWithFee * aReserveOut;
-        uint256 lDenominator = aReserveIn * FEE_ACCURACY + lAmountInWithFee;
+        uint256 lAmountInWithFee = aAmountIn.mulDiv(FEE_ACCURACY - aSwapFee, 1);
+        uint256 lNumerator = lAmountInWithFee.mulDiv(aReserveOut, 1);
+        uint256 lDenominator = aReserveIn.mulDiv(FEE_ACCURACY, 1) + lAmountInWithFee;
         rAmountOut = lNumerator / lDenominator;
     }
 
