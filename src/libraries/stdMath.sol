4d7c149e1bf9bf822d226e9c38126a5bb09d63dd
A.L <xenide@users.noreply.github.com>
2023-05-13 13:11:54 +0100

    feat: mulDiv for stdMath

--- src/libraries/stdMath.sol
+++ src/libraries/stdMath.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
+import { Math } from "@openzeppelin/utils/math/Math.sol";
+
 // Our gas-optimized implementation of forge-std's stdMath library
 // We should inherit and override the relevant functions in the future when they make them virtual
 library stdMath {
+    using Math for uint256;
+
     uint256 private constant WAD = 1e18; // The scalar of ETH and most ERC20s.
 
     function delta(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -16,6 +20,6 @@ library stdMath {
 
     function percentDelta(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 absDelta = delta(a, b);
-        return absDelta * WAD / b;
+        return absDelta.mulDiv(WAD, b);
     }
 }
