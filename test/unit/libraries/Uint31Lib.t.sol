cff3a11a918ace1b8b77133287242a8b8940301e
A.L <xenide@users.noreply.github.com>
2023-05-25 21:54:17 +0100

    fix: rm blank lines

--- test/unit/libraries/Uint31Lib.t.sol
+++ test/unit/libraries/Uint31Lib.t.sol
@@ -6,8 +6,6 @@ import "forge-std/Test.sol";
 import { Uint31Lib } from "src/libraries/Uint31Lib.sol";
 
 contract Uint31LibTest is Test {
-
-
     function testSubtract() external {
         uint32 lA = 4;
         uint32 lB = 0x7FFFFFFF; // max value of uint31
