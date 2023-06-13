5453ea5c738e1b755e218e1f4ebdd8c044aa1c62
A.L <xenide@users.noreply.github.com>
2023-06-13 07:54:16 +0200

    refactor: `_ensureDeployerExists` to take in variable address

--- script/BaseScript.sol
+++ script/BaseScript.sol
@@ -9,13 +9,13 @@ import { ReservoirDeployer } from "src/ReservoirDeployer.sol";
 contract BaseScript is Script {
     ReservoirDeployer internal _deployer;
 
-    function _ensureDeployerExists(uint256 aPrivateKey) internal {
+    function _ensureDeployerExists(uint256 aPrivateKey, address aGuardian1, address aGuardian2, address aGuardian3) internal {
         bytes memory lInitCode = abi.encodePacked(type(ReservoirDeployer).creationCode);
         lInitCode = abi.encodePacked(lInitCode, abi.encode(msg.sender, msg.sender, msg.sender));
         address lDeployer = Create2Lib.computeAddress(CREATE2_FACTORY, lInitCode, bytes32(0));
         if (lDeployer.code.length == 0) {
             vm.broadcast(aPrivateKey);
-            _deployer = new ReservoirDeployer{salt: bytes32(0)}(msg.sender, msg.sender, msg.sender);
+            _deployer = new ReservoirDeployer{salt: bytes32(0)}(aGuardian1, aGuardian2, aGuardian3);
             require(address(_deployer) == lDeployer, "CREATE2 ADDRESS MISMATCH");
             require(address(_deployer) != address(0), "DEPLOY FACTORY FAILED");
         } else {
