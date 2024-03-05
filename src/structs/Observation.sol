a01bff65d0fc37af22cbdb6a0488a3e506933474
A.L <xenide@users.noreply.github.com>
2024-03-05 22:24:50 +0800

    docs: update comment with minimum value of log value

--- src/structs/Observation.sol
+++ src/structs/Observation.sol
@@ -3,13 +3,14 @@ pragma solidity ^0.8.0;
 
 struct Observation {
     // natural log (ln) of the raw instant price. Fits into int24 as the max value is 1353060
+    // while the minimum value is -414465
     // See `LogCompressionTest` for more info
     int24 logInstantRawPrice;
     // natural log (ln) of the clamped instant price
     int24 logInstantClampedPrice;
 
     // natural log (ln) of the raw accumulated price (token1/token0)
-    // in the case of maximum price supported by the oracle (~2.87e56 == e ** 135.3060)
+    // in the case of maximum price supported by the oracle (~5.79e58 == e ** 135.3060)
     // (1353060) 21 bits multiplied by 32 bits of the timestamp gives 53 bits
     // which fits into int88
     int88 logAccRawPrice;
