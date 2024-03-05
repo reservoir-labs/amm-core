666204fa54efefdc1c3ccd8a326282fb22520118
A.L <xenide@users.noreply.github.com>
2024-03-05 22:17:52 +0800

    fix: rm `SafeCastLib` import

--- src/curve/constant-product/ConstantProductPair.sol
+++ src/curve/constant-product/ConstantProductPair.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-import { SafeCastLib } from "solady/utils/SafeCastLib.sol";
-
 import { Math } from "@openzeppelin/utils/math/Math.sol";
 import { FixedPointMathLib } from "solady/utils/FixedPointMathLib.sol";
 
