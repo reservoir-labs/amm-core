e9d2c1a4ceb33989c7b19638c9f7ad66474dd949
A.L <xenide@users.noreply.github.com>
2024-03-07 09:30:24 +0800

    fix: lint

--- test/interfaces/IUSDC.sol
+++ test/interfaces/IUSDC.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 interface IUSDC {
-    function masterMinter() view external returns (address);
+    function masterMinter() external view returns (address);
     function mint(address dst, uint256 amount) external;
     function configureMinter(address minter, uint256 minterAllowedAmount) external returns (bool);
 }
