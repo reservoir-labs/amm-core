20b95e115be8bbcef2c4f85cbc458fe6134effcd
A.L <xenide@users.noreply.github.com>
2024-03-07 09:57:00 +0800

    test: update storage slot for `vm.store`

--- test/unit/StablePair.t.sol
+++ test/unit/StablePair.t.sol
@@ -924,7 +924,7 @@ contract StablePairTest is BaseTest {
         bytes32 lEncoded = bytes32(abi.encodePacked(lLastInvariantAmp, lLastInvariant));
         // hardcoding the slot for now as there is no way to access it publicly
         // this will break when we change the storage layout
-        vm.store(address(_stablePair), bytes32(uint256(18)), lEncoded);
+        vm.store(address(_stablePair), bytes32(uint256(17)), lEncoded);
 
         // ensure that the iterative function that _mintFee calls reverts with the adulterated values
         vm.prank(address(_stablePair));
